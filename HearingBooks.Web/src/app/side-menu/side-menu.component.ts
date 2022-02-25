42772d3050ff6cf4ecf98c7423b9ad8e756b200a
lukaszmojek <lukasz.mojek@gmail.com>
2022-02-25 20:51:14 +0100

    Install eslint and fix linted problems

--- HearingBooks.Web/src/app/side-menu/side-menu.component.ts
+++ HearingBooks.Web/src/app/side-menu/side-menu.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'hb-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.scss']
 })
-export class SideMenuComponent implements OnInit {
+export class SideMenuComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
