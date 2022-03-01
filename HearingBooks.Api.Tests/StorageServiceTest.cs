c5d6a042763256e73e0b2cbab8fcca16b12f6cab
lukaszmojek <lukasz.mojek@gmail.com>
2022-03-01 18:33:35 +0100

    Refactor StorageServiceTest

--- HearingBooks.Api.Tests/StorageServiceTest.cs
+++ HearingBooks.Api.Tests/StorageServiceTest.cs
@@ -1,37 +1,33 @@
-using System.ComponentModel;
+using System;
+using System.Threading;
 using System.Threading.Tasks;
 using HearingBooks.Api.Configuration;
 using HearingBooks.Api.Storage;
+using HearingBooks.Tests.Core;
 using Microsoft.Extensions.Configuration;
 using Xunit;
 
 namespace HearingBooks.Api.Tests;
 
 public class StorageServiceTest {
-     [Category("StorageService")]
-     public class StorageServiceTestBase
+     public class StorageServiceTestBase : TestBase
      {
-         protected readonly string _containerName = "test-container-name";
+         protected readonly string _containerName = Guid.NewGuid().ToString();
          protected IStorageService StorageService;
          
          public async Task RunAsync()
          {
-             await PrepareDependencies();
-             await ExecuteTest();
+             await PrepareEnvironmentForTest();
+             await ExecuteTestAsync();
              await Cleanup();
          }
-         
+
          public virtual Task ExecuteTestAsync()
          {
              return Task.CompletedTask;
          }
     
-         public virtual Task ExecuteTest()
-         {
-             return Task.CompletedTask;
-         }
-    
-         private async Task PrepareDependencies()
+         private async Task PrepareEnvironmentForTest()
          {
              var configuration = new ConfigurationManager()
                  .AddJsonFile("appsettings.Development.json")
@@ -40,23 +36,34 @@ public class StorageServiceTest {
              var apiConfiguration = new ApiConfiguration(configuration);
     
              StorageService = new StorageService(apiConfiguration);
+
+             await DeleteContainerIfExists();
          }
     
          private async Task Cleanup()
          {
-             
+             await DeleteContainerIfExists();
+         }
+         
+         private async Task DeleteContainerIfExists()
+         {
+             if (await StorageService.ContainerExistsAsync(_containerName))
+             {
+                 await StorageService.DeleteContainerAsync(_containerName);
+             }
          }
      }
-    
-    public class When_CreateContainer_Is_Called_Should_Create_Container_In_AzureBlobStorage : StorageServiceTestBase
+     
+    public class When_ContainerExistsAsync_Is_Called_Should_Return_False_When_Container_Does_Not_Exists : StorageServiceTestBase
     {
+        //TODO: Think how this can be refactored
         [Fact]
         public async Task InvokeTest()
         {
             await RunAsync();
         }
         
-        public override async Task ExecuteTest()
+        public override async Task ExecuteTestAsync()
         {
             var containerExistsBeforeCallingCreateContainer = await StorageService.ContainerExistsAsync(_containerName);
             var _ = await StorageService.CreateContainerAsync(_containerName);
@@ -67,117 +74,90 @@ public class StorageServiceTest {
         }
     }
 
-    // [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
-    // public class InitializeStorageServiceAndCleanupAfterTest : BeforeAfterTestAttribute
-    // {
-    //     protected readonly string _containerName;
-    //
-    //     public InitializeStorageServiceAndCleanupAfterTest(string containerName)
-    //     {
-    //         _containerName = containerName;
-    //     }
-    //
-    //     protected IStorageService StorageService { get; set; }
-    //     
-    //     public override void Before(MethodInfo methodUnderTest)
-    //     {
-    //         StorageService = StorageHelpers.CresteStorageService();
-    //         DeleteContainerIfExists();
-    //     }
-    //     
-    //     public override void After(MethodInfo methodUnderTest)
-    //     {
-    //         DeleteContainerIfExists();
-    //     }
-    //
-    //     private async Task DeleteContainerIfExists()
-    //     {
-    //         if (await StorageService.ContainerExistsAsync(_containerName))
-    //         {
-    //             await StorageService.DeleteContainerAsync(_containerName);
-    //         }
-    //     }
-    // }
-    //
-    // [InitializeStorageServiceAndCleanupAfterTest]
-    // public class When_ContainerExistsAsync_Is_Called_Should_Return_False_When_Container_Does_Not_Exists
-    // {
-    //     protected readonly string _containerName = Guid.NewGuid().ToString();
-    //     
-    //     [Fact]
-    //     public async Task ExecuteTest()
-    //     {
-    //         var storageService = StorageHelpers.CresteStorageService();
-    //         
-    //         var containerExists = await storageService.ContainerExistsAsync(_containerName);
-    //
-    //         Assert.False(containerExists);
-    //     }
-    // }
-    //
-    // [InitializeStorageServiceAndCleanupAfterTest]
-    // public class When_ContainerExistsAsync_Is_Called_Should_Return_True_When_Container_Exists
-    // {
-    //     protected readonly string _containerName = Guid.NewGuid().ToString();
-    //     
-    //     [Fact]
-    //     public async Task ExecuteTest()
-    //     {
-    //         var storageService = StorageHelpers.CresteStorageService();
-    //         
-    //         await storageService.CreateContainerAsync(_containerName);
-    //         var containerExists = await storageService.ContainerExistsAsync(_containerName);
-    //
-    //         Assert.True(containerExists);
-    //     }
-    // }
-    //
-    // [InitializeStorageServiceAndCleanupAfterTest]
-    // public class When_CreateContainer_Is_Called_Should_Create_Container_In_AzureBlobStorage
-    // {
-    //     protected readonly string _containerName = Guid.NewGuid().ToString();
-    //     
-    //     [Fact]
-    //     public async Task ExecuteTest()
-    //     {
-    //         var storageService = StorageHelpers.CresteStorageService();
-    //         
-    //         var containerExistsBeforeCallingCreateContainer = await storageService.ContainerExistsAsync(_containerName);
-    //         var _ = await storageService.CreateContainerAsync(_containerName);
-    //         var containerExistsAfterCallingCreateContainer = await storageService.ContainerExistsAsync(_containerName);
-    //
-    //         Assert.False(containerExistsBeforeCallingCreateContainer);
-    //         Assert.True(containerExistsAfterCallingCreateContainer);
-    //     }
-    // }
-    //
-    // [InitializeStorageServiceAndCleanupAfterTest]
-    // public class When_UploadBlob_Is_Called_Should_Upload_Blob_To_Container_In_AzureBlobStorage : TestBase
-    // {
-    //     private readonly string _containerName = Guid.NewGuid().ToString();
-    //     private readonly string _fileName = "opowiesci-niesamowite.epub";
-    //     
-    //     [Fact]
-    //     public async Task ExecuteTest()
-    //     {
-    //         var StorageService = StorageHelpers.CresteStorageService();
-    //         
-    //         var blobContainerClient = await StorageService.CreateContainerAsync(_containerName);
-    //         var containerExistsAfterCallingCreateContainer = await StorageService.ContainerExistsAsync(_containerName);
-    //         var _ = await StorageService.UploadBlobAsync(
-    //             blobContainerClient,
-    //             _fileName,
-    //             GetPathForTestFile(_fileName)
-    //         );
-    //
-    //         var blobExists = await StorageService.BlobExistsAsync(
-    //             blobContainerClient,
-    //             _fileName,
-    //             CancellationToken.None
-    //         );
-    //         
-    //         Assert.True(containerExistsAfterCallingCreateContainer);
-    //         Assert.True(blobExists);
-    //     }
-    // }
+    public class When_ContainerExistsAsync_Is_Called_Should_Return_True_When_Container_Exists : StorageServiceTestBase
+    {
+        [Fact]
+        public async Task InvokeTest()
+        {
+            await RunAsync();
+        }
+        
+        public override async Task ExecuteTestAsync()
+        {
+            await StorageService.CreateContainerAsync(_containerName);
+            var containerExists = await StorageService.ContainerExistsAsync(_containerName);
+    
+            Assert.True(containerExists);
+        }
+    }
+
+    public class When_CreateContainer_Is_Called_Should_Create_Container_In_AzureBlobStorage : StorageServiceTestBase
+    {
+        [Fact]
+        public async Task InvokeTest()
+        {
+            await RunAsync();
+        }
+        
+        public override async Task ExecuteTestAsync()
+        {
+            var containerExistsBeforeCallingCreateContainer = await StorageService.ContainerExistsAsync(_containerName);
+            var _ = await StorageService.CreateContainerAsync(_containerName);
+            var containerExistsAfterCallingCreateContainer = await StorageService.ContainerExistsAsync(_containerName);
+    
+            Assert.False(containerExistsBeforeCallingCreateContainer);
+            Assert.True(containerExistsAfterCallingCreateContainer);
+        }
+    }
+
+    public class When_UploadBlob_Is_Called_Should_Upload_Blob_To_Container_In_AzureBlobStorage : StorageServiceTestBase
+    {
+        private readonly string _fileName = "opowiesci-niesamowite.epub";
+        
+        [Fact]
+        public async Task InvokeTest()
+        {
+            await RunAsync();
+        }
+        
+        public override async Task ExecuteTestAsync()
+        {
+            var blobContainerClient = await StorageService.CreateContainerAsync(_containerName);
+            var containerExistsAfterCallingCreateContainer = await StorageService.ContainerExistsAsync(_containerName);
+            var _ = await StorageService.UploadBlobAsync(
+                blobContainerClient,
+                _fileName,
+                GetPathForTestFile(_fileName)
+            );
+    
+            var blobExists = await StorageService.BlobExistsAsync(
+                blobContainerClient,
+                _fileName,
+                CancellationToken.None
+            );
+            
+            Assert.True(containerExistsAfterCallingCreateContainer);
+            Assert.True(blobExists);
+        }
+    }
+    
+    public class When_DeleteContainerAsync_Is_Called_Should_Delete_Container_From_AzureBlobStorage : StorageServiceTestBase
+    {
+        [Fact]
+        public async Task InvokeTest()
+        {
+            await RunAsync();
+        }
+        
+        public override async Task ExecuteTestAsync()
+        {
+            var _ = await StorageService.CreateContainerAsync(_containerName);
+            var containerExistsBeforeCallingDeleteContainer = await StorageService.ContainerExistsAsync(_containerName);
+            await StorageService.DeleteContainerAsync(_containerName);
+            var containerExistsAfterCallingDeleteContainer = await StorageService.ContainerExistsAsync(_containerName);
+
+            Assert.True(containerExistsBeforeCallingDeleteContainer);
+            Assert.False(containerExistsAfterCallingDeleteContainer);
+        }
+    }
 }
