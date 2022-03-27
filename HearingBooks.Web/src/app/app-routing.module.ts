f89c2a18b0ac152d569eb0c85804fda801da666a
lukasz.mojek <lukasz.mojek@gmail.com>
2022-03-27 15:15:47 +0200

    Change project structure, split into users

--- HearingBooks.Web/src/app/app-routing.module.ts
+++ HearingBooks.Web/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { SideMenuComponent } from './core/side-menu/side-menu.component'
 
-const routes: Routes = []
+const routes: Routes = [
+  {
+    'path': 'dashboard',
+    'component': SideMenuComponent
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
