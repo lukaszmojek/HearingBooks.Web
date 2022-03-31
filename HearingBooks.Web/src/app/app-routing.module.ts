ab65dae4ea511d1ad7f943ff80815293bee18ecd
lukasz.mojek <lukasz.mojek@gmail.com>
2022-03-31 08:00:56 +0200

    Combine Core and Shared module

--- HearingBooks.Web/src/app/app-routing.module.ts
+++ HearingBooks.Web/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
-import { SideMenuComponent } from './core/side-menu/side-menu.component'
+import { SideMenuComponent } from './shared/side-menu/side-menu.component'
 
 const routes: Routes = [
   {
--- HearingBooks.Web/src/app/shared/shared.module.ts
+++ HearingBooks.Web/src/app/shared/shared.module.ts
@@ -1,8 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core'
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { MaterialModule } from '../material.module';
+import { LoggedUserComponent } from './logged-user/logged-user.component';
+import { MainAppComponent } from './main-app/main-app.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
 
+const components = [
+  MainAppComponent,
+  SideMenuComponent,
+  LoggedUserComponent
+]
+
+const modules = [
+  CommonModule,
+  MaterialModule,
+  RouterModule,
+  TranslateModule
+]
 
 @NgModule({
-  declarations: [],
-  exports: []
+  declarations: [
+    ...components,
+  ],
+  imports: [
+    ...modules,
+  ],
+  exports: [
+    ...components,
+    ...modules,
+  ]
 })
 export class SharedModule {}
