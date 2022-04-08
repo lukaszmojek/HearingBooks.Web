85a500061eada6050399601dba4ce5aadc782dbd
lukasz.mojek <lukasz.mojek@gmail.com>
2022-04-08 17:42:31 +0200

    Implement canActivate in the guard

--- HearingBooks.Web/src/app/auth/auth.guard.ts
+++ HearingBooks.Web/src/app/auth/auth.guard.ts
@@ -1,27 +1,37 @@
 import { Injectable } from '@angular/core'
-import { CanLoad, Route, Router, UrlSegment, UrlTree } from '@angular/router'
-import { Observable } from 'rxjs'
-import { AuthorizationService } from './authorization.service'
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router'
+import { Store } from '@ngrx/store'
+import { catchError, map, Observable } from 'rxjs'
+import { IAuthState } from './auth.reducer'
+import { selectIsLoggedIn } from './auth.selectors'
 
+type GuardOutcome = boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree>
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanLoad {
-  constructor(private auth: AuthorizationService, private router: Router) {}
+export class AuthGuard implements CanLoad, CanActivate {
+  constructor(private store$: Store<IAuthState>, private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): GuardOutcome {
+    return this.guard()
+  }
 
   canLoad(
     route: Route,
     segments: UrlSegment[]
-  ):
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree>
-    | boolean
-    | UrlTree {
-    if (this.auth.isAuthorized) {
-      return true
-    }
+  ): GuardOutcome {
+    return this.guard()
+  }
 
-    this.router.navigate(['login'])
-    return false
+  private guard(): GuardOutcome {
+    //TODO: Check if this will work properly with iif from rxjs
+    return this.store$.select(selectIsLoggedIn)
+    // .pipe(
+    //   map(isLoggedIn => {
+    //     if (isLoggedIn) {
+    //       return isLoggedIn
+    //     }
+    //   })
+    // )
   }
 }
