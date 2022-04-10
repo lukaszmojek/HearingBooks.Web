930c600ff3877beab0ed9eba896d5929888ab4f6
lukasz.mojek <lukasz.mojek@gmail.com>
2022-04-10 20:01:43 +0200

    Fix AuthGuard not working for childern of lazy loaded module

--- HearingBooks.Web/src/app/auth/auth.guard.ts
+++ HearingBooks.Web/src/app/auth/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core'
-import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router'
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router'
 import { Store } from '@ngrx/store'
 import { catchError, map, Observable } from 'rxjs'
 import { IAuthState } from './auth.reducer'
@@ -9,17 +9,21 @@ type GuardOutcome = boolean | UrlTree | Observable<boolean | UrlTree> | Promise<
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanLoad, CanActivate {
+export class AuthGuard implements CanLoad, CanActivate, CanActivateChild {
   constructor(private store$: Store<IAuthState>, private router: Router) { }
-
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): GuardOutcome {
-    return this.guard()
-  }
 
   canLoad(
     route: Route,
     segments: UrlSegment[]
   ): GuardOutcome {
+    return this.guard()
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): GuardOutcome {
+    return this.guard()
+  }
+
+  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): GuardOutcome {
     return this.guard()
   }
 
