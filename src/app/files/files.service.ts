b3ad328555141144ab49897f2870ad0546bcf1be
lukasz.mojek <lukasz.mojek@gmail.com>
2022-05-01 20:53:56 +0200

    Refactor FileService

--- src/app/files/files.service.ts
+++ src/app/files/files.service.ts
@@ -11,8 +11,18 @@ export class FilesService {
   constructor(private http: HttpClient) { }
 
   downloadTextSynthesis$(synthesisId: string, filename: string): void {
-    this.http.get(ApiEndpoints.textSyntheses.download(synthesisId), { responseType: "blob" }).subscribe(x => {
-      saveAs(x, filename);
+    const downloadUrl = ApiEndpoints.textSyntheses.download(synthesisId)
+    this.downloadFile(downloadUrl, filename)
+  }
+
+  downloadDialogueSynthesis$(synthesisId: string, filename: string): void {
+    const downloadUrl = ApiEndpoints.dialogueSyntheses.download(synthesisId)
+    this.downloadFile(downloadUrl, filename)
+  }
+
+  private downloadFile(downloadUrl: string, filename: string): void {
+    this.http.get(downloadUrl, { responseType: "blob" }).subscribe(x => {
+      saveAs(x, filename)
     })
   }
 }
