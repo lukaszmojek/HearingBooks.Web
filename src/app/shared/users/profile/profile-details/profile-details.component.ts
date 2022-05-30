f2d126333e7d605eea67251785d25e22601be042
lukaszmojek <lukasz.mojek@gmail.com>
2022-05-30 18:35:44 +0200

    Update user profile view

--- src/app/shared/users/profile/profile-details/profile-details.component.ts
+++ src/app/shared/users/profile/profile-details/profile-details.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
+import { FormGroup, FormControl } from '@angular/forms'
+import { Store } from '@ngrx/store'
+import { IPreferencesState } from 'src/app/preferences/preferences.reducer'
+import { selectInnerCardType, selectLanguage } from 'src/app/preferences/preferences.selectors'
+import { CardType } from 'src/app/shared/card/card-type'
+import StoreConnectedComponent from 'src/app/shared/store-connected.component'
 
 @Component({
   selector: 'hb-profile-details',
   templateUrl: './profile-details.component.html',
   styleUrls: ['./profile-details.component.scss'],
 })
-export class ProfileDetailsComponent {
-  constructor() {}
+export class ProfileDetailsComponent extends StoreConnectedComponent<IPreferencesState> {
+  @Input() profileDetailsFormGroup: FormGroup
+
+  public innerCardBorder = false
+  public innerCardType: CardType
+  public selectedLanguage: string
+
+  public get firstNameFormControl(): FormControl {
+    return this.profileDetailsFormGroup.get('firstName') as FormControl
+  }
+
+  public get lastNameFormControl(): FormControl {
+    return this.profileDetailsFormGroup.get('lastName') as FormControl
+  }
+
+  public get emailFormControl(): FormControl {
+    return this.profileDetailsFormGroup.get('email') as FormControl
+  }
+
+  constructor(store$: Store<IPreferencesState>) {
+    super(store$)
+    this.safeSelect$(selectInnerCardType).subscribe(
+      x => (this.innerCardType = x)
+    )
+    this.safeSelect$(selectLanguage).subscribe(x => (this.selectedLanguage = x))
+  }
 }
--- src/app/shared/users/profile/profile.component.ts
+++ src/app/shared/users/profile/profile.component.ts
@@ -6,7 +6,10 @@ import {
   FormGroup,
 } from '@angular/forms'
 import { Store } from '@ngrx/store'
+import { selectUserDetails } from 'src/app/auth/auth.selectors'
+import { IUser } from 'src/app/auth/models'
 import { selectIsAcrylicEnabled } from 'src/app/preferences/preferences.selectors'
+import { IUIState } from 'src/app/ui/ui.reducer'
 import AcrylicAwareComponent from '../../acrylic/acrylic-aware.component'
 import { AcrylicService } from '../../acrylic/acrylic.service'
 import { IMainComponent } from '../../main-component.interface'
@@ -30,6 +33,12 @@ export class ProfileComponent
     preferences: 'preferences',
   }
 
+  private detailsNames = {
+    firstName: 'firstName',
+    lastName: 'lastName',
+    email: 'email',
+  }
+
   private preferencesNames = {
     acrylic: 'acrylic',
     language: 'langauge',
@@ -49,6 +58,37 @@ export class ProfileComponent
       this.acrylicFormControl.setValue(this.isAcrylicEnabled)
       this.profileFormGroup.updateValueAndValidity()
     })
+    this.safeSelect$(selectUserDetails).subscribe((details: IUser) => {
+      this.firstNameFormControl.setValue(details.firstName)
+      this.firstNameFormControl.disable()
+      this.lastNameFormControl.setValue(details.lastName)
+      this.lastNameFormControl.disable()
+      this.emailFormControl.setValue(details.email)
+      this.emailFormControl.disable()
+      this.profileFormGroup.updateValueAndValidity()
+    })
+  }
+
+  get profileDetailsFormGroup(): FormGroup {
+    return this.getFormPart(this.formGroupNames.details) as FormGroup
+  }
+
+  private get firstNameFormControl(): FormControl {
+    return this.getFormPart(this.formGroupNames.details).get(
+      this.detailsNames.firstName
+    ) as FormControl
+  }
+
+  private get lastNameFormControl(): FormControl {
+    return this.getFormPart(this.formGroupNames.details).get(
+      this.detailsNames.lastName
+    ) as FormControl
+  }
+
+  private get emailFormControl(): FormControl {
+    return this.getFormPart(this.formGroupNames.details).get(
+      this.detailsNames.email
+    ) as FormControl
   }
 
   get preferencesFormGroup(): FormGroup {
@@ -67,7 +107,17 @@ export class ProfileComponent
 
   private createProfileFormGroup(): void {
     this.profileFormGroup = this.formBuilder.group({
-      [this.formGroupNames.details]: this.formBuilder.group({}),
+      [this.formGroupNames.details]: this.formBuilder.group({
+        [this.detailsNames.firstName]: this.formBuilder.control(
+          ''
+        ),
+        [this.detailsNames.lastName]: this.formBuilder.control(
+          ''
+        ),
+        [this.detailsNames.email]: this.formBuilder.control(
+          ''
+        ),
+      }),
       [this.formGroupNames.preferences]: this.formBuilder.group({
         [this.preferencesNames.acrylic]: this.formBuilder.control(
           this.isAcrylicEnabled
