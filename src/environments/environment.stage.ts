3bd00e5e34300d5158fdbbed1c55a669034decce
lukaszmojek <lukasz.mojek@gmail.com>
2022-10-11 22:38:41 +0200

    Add missing baseSignalRUrl to configuration

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -3,4 +3,5 @@ import { IEnvironment } from './environment.interface'
 export const environment: IEnvironment = {
   production: false,
   baseApiUrl: 'stage',
+  baseSignalRUrl: '',
 }
--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -3,4 +3,5 @@ import { IEnvironment } from './environment.interface'
 export const environment: IEnvironment = {
   production: false,
   baseApiUrl: 'https://hearing-books-api-test.azurewebsites.net',
+  baseSignalRUrl: '',
 }
